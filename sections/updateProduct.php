8672a8a9e014ffa16290926d7ddb1d81a66619f2
Anasse LAARABI <contact.laarabi@gmail.com>
2023-05-10 15:18:49 +0100

    Update updateProduct.php

--- sections/updateProduct.php
+++ sections/updateProduct.php
@@ -1,8 +1,17 @@
 <?php
 
+
+if ($_POST){
+header('Location: /');
+}
+
+?>
+
+<?php
+
 $productTitle = $_GET['product'];
 $pageTitle = "Update" . $productTitle;
-require 'header.php';
+include 'header.php';
 require '../functions/db.php';
 
 // echo "<h1>$productTitle</h1>";
@@ -11,7 +20,7 @@ $sql = "SELECT * FROM products WHERE productName = \"$productTitle\" ";
 $res = mysqli_query($conn, $sql);
 
 $product = mysqli_fetch_assoc($res);
-
+/*
 echo "<h1>{$product['productName']}</h1>";
 echo "<img src=\"../products/$product[productPicture]\">";
 
@@ -20,48 +29,56 @@ echo "<pre>";
 print_r($product);
 
 echo "</pre>";
-
+*/
 
 ?>
 
 
-<form method="post" enctype="multipart/form-data">
-  <label for="productPicture">Add the picture of your product</label>
-  <input type="file" name="productPicture" id="productPicture" value="<?= $product['productPicture']?>">
-  <label for="productName">Add the name of your product</label>
-  <input type="text" name="productName" id="productName" value="<?= $product['productName']?>">
+<section class="addProduct">
+<h1><?= $product['productName']?></h1>
+<img src="../products/<?= $product['productPicture']?>">
+  <form method="post" enctype="multipart/form-data">
+      <div class="mb-3">
+        <label class="form-label" for="productName">Add the name of your product</label>
+        <input class="form-control" type="text" name="productName" id="productName" value="<?= $product['productName']?>">
+      </div>
+      <div class="mb-3">
+        <label class="form-label" for="type">Type</label>
 
+        <?php
+          $sql = "SELECT * FROM types";
+          $res = mysqli_query($conn, $sql);
+        ?>
 
-  <label for="type">Type</label>
-  <?php
-    $sql = "SELECT * FROM types";
-    $res = mysqli_query($conn, $sql);
-  ?>
-
-    <select id="type" name="type">
-      <?php while( $types = mysqli_fetch_assoc($res) ):?>
-        <option
-          <?= ($product['type_id'] == $types['id'])? 'selected="selected"':''?>
-          value=<?=$types['id']?>
-        >
-        <?= $types['type']?>
-      </option>
-
-      <?php endwhile?>
-    </select>
-
-
-  <label for="quantity">Quantity</label>
-  <input type="quantity" name="quantity" id="quantity" value="<?= $product['quantity']?>">
-  <label for="price">Price</label>
-  <input type="price" name="price" id="price" value="<?= $product['price']?>">
-  <textarea id="productDescription" name="productDescription"><?= $product['productDescription']?></textarea>
-  <button type="submit">Submit</button>
-  <a href="/"> Cancel </a>
-</form>
-
+          <select class="form-select" id="type" name="type">
+            <?php while( $types = mysqli_fetch_assoc($res) ):?>
+              <option <?= ($product['type_id'] == $types['id'])? 'selected="selected"':''?> value=<?=$types['id']?>>
+                <?= $types['type']?>
+              </option>
+            <?php endwhile?>
+          </select>
+      </div>
+      <div class="mb-3">
+        <label class="form-label" for="quantity">Quantity</label>
+        <input class="form-control" type="quantity" name="quantity" id="quantity" value="<?= $product['quantity']?>">
+      </div>
+      <div class="mb-3">
+        <label class="form-label" for="price">Price</label>
+        <input class="form-control" type="price" name="price" id="price" value="<?= $product['price']?>">
+      </div>
+      <div class="mb-3">
+        <textarea class="form-control" id="productDescription" name="productDescription" placeholder="Add a description for your product"><?= $product['productDescription']?></textarea>
+      </div>
+      <div class="btn-grp">
+        <button class="btn btn-success" type="submit">Submit</button>
+        <a class="btn btn-secondary" href="/"> Cancel </a>
+      </div>
+  </form>
+</section>
 
 <?php
+
+
 if ($_POST){
 
 $productName = $_POST['productName'];
@@ -76,6 +93,7 @@ $sql = "UPDATE products
         WHERE productName = \"$productTitle\"";
 
 mysqli_query($conn, $sql);
-
-header("Location: /");
 }
+
+?>
+
